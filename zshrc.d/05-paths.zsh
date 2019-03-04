65c769130022aa6f36fc4676b5d89d66534bf9b9
Lee Robert <lrobert@leerobert.ca>
2019-03-04 14:19:34 -0500

    Removes work specific paths

--- zshrc.d/05-paths.zsh
+++ zshrc.d/05-paths.zsh
@@ -1,8 +1,5 @@
 #!/bin/zsh
 
-export PATH="$PATH:/home/lrobert/Applications/PhpStorm-181.5281.35/bin"
-export PATH="$HOME/dev/ratehub/dev-scripts/bin:$PATH"
 export PATH="$HOME/.config/composer/vendor/bin:$PATH"
 export PATH="$HOME/.voziv/bin:$PATH"
-export PATH="$HOME/.ratehub/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
