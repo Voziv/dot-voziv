4f5571d062f3779a548185d2da7aa51ce5976b21
Lee Robert <lrobert@leerobert.ca>
2019-12-16 11:13:22 -0500

    Made autocompletions more dynamic

--- zshrc.d/03-completions.zsh
+++ zshrc.d/03-completions.zsh
@@ -3,5 +3,21 @@ scriptdir=${0:a:h}
 completions_dir="$scriptdir/completions.d"
 fpath=($completions_dir $fpath)
 
+if type -p "kubectl" &> /dev/null; then
+  source <(kubectl completion zsh)
+fi
+
+if type -p "doctl" &> /dev/null; then
+  source <(doctl completion zsh)
+fi
+
+if type -p "helm" &> /dev/null; then
+  source <(helm completion zsh)
+fi
+
+if type -p "minikube" &> /dev/null; then
+  source <(minikube completion zsh)
+fi
+
 # Reload the zsh-completions
 autoload -U compinit && compinit
