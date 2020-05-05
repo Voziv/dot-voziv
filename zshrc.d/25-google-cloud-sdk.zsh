65d6d66eaf67d1c4a9c8fbc8ccf3aa892737f1b4
Lee Robert <lrobert@leerobert.ca>
2020-05-05 07:42:54 -0400

    Add google cloud sdk includes

---
+++ zshrc.d/25-google-cloud-sdk.zsh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+[ -f "$HOME/google-cloud-sdk/path.zsh.inc" ] && . $HOME/google-cloud-sdk/path.zsh.inc
+[ -f "$HOME/google-cloud-sdk/completion.zsh.inc" ] && . $HOME/google-cloud-sdk/completion.zsh.inc
