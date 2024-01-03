8661eef464c83d5a3ff03fb92c79ce18eec26497
Lee Robert <lrobert@leerobert.ca>
2024-01-03 12:49:21 -0500

    feat(nvim): start journey to using nvim as an IDE in 2024

--- install.sh
+++ install.sh
@@ -5,11 +5,28 @@ script_dir=$(dirname ${BASH_SOURCE[0]})
 script_dir=$(realpath ${script_dir})
 if [[ ! -d "$script_dir" ]]; then script_dir="$PWD"; fi
 
+runDate=$(date -d "today" +"%Y%m%d%H%M%S")
 
 function install() {
+    ensure_user_bin
+    install_symlinks
+    install_profile
+    install_zshrc
+}
+
+function ensure_user_bin() {
+    echo "Ensuring $HOME/bin is created"
+    mkdir -p "$HOME/bin"
+}
+
+function install_symlinks() {
+    # Clean up old items, useful for upgrading other machines.
+    rm_symlink "$HOME/.nvimrc" "$script_dir/.vimrc"
+    rm_symlink "$HOME/.vimrc" "$script_dir/.vimrc"
+    
+    # Add symlinks
+    do_symlink "$HOME/.config/nvim" "$script_dir/nvim"
     do_symlink "$HOME/.gitconfig" "$script_dir/git/gitconfig"
-    do_symlink "$HOME/.vimrc" "$script_dir/.vimrc"
-    do_symlink "$HOME/.nvimrc" "$script_dir/.vimrc"
     do_symlink "$HOME/.tmux.conf" "$script_dir/.tmux.conf"
     do_symlink "$HOME/.screenrc" "$script_dir/.screenrc"
 }
@@ -19,28 +36,60 @@ function do_symlink() {
     local target=$2
 
     if [ -L "$link" ]; then
-        rm "$link"
-#        echo  "rm $link"
-    else
-        if [ -f "$link" ]; then
-            echo "$link is a file. Not removing it to be safe."
+        if [ "$(readlink -- "$link")" = "$target" ]; then
+            echo "$link already points to $target"
+            return
         fi
+        
+        echo "Removing $link that points to `readlink $link`. Reason: Installing new link"
+        rm "$link"
+    else
+        if [ -e "$link" ]; then
+            echo "$link exists and is not a symlink. Not removing it to be safe."
+            return
+        fi
+
+        echo "Symlinking $link to $target"
     fi
 
-    # ln -s <target> <link>
     ln -s "$target" "$link"
-#    echo "ln -s $target $link"
+}
+
+function rm_symlink() {
+    local link=$1
+    local expectedTarget=$2
+
+    if [ -L "$link" ]; then
+        echo "Removing $link that points to `readlink $link`. Reason: Deprecated"
+        rm "$link"
+    fi
 }
 
 
 function install_profile() {
-    # TODO lrobert: Make sure .profile sources "$script_dir/profile"
-    echo "Installing profile"
+    sourceString="source $script_dir/.profile"
+    if grep -q -F "$sourceString" "$HOME/.profile"; then 
+        echo ".profile already configured. Skipping."
+        return
+    fi
+
+    echo ".profile not configured Installing .profile"
+    echo "$sourceString" >> "$HOME/.profile"
 }
 
 function install_zshrc() {
-    # TODO lrobert: Make sure .profile sources "$script_dir/profile"
-    echo "Installing zshrc"
+    sourceString="source $script_dir/.zshrc"
+    if grep -q -F "$sourceString" "$HOME/.zshrc"; then 
+        echo ".zshrc already configured. Skipping."
+        return
+    fi
+    
+    echo ".zshrc not configured Installing .zshrc"
+    if [ -f "$HOME/.zshrc" ]; then
+        mv "$HOME/.zshrc" "$HOME/.zshrc.pre-dot-voziv-${runDate}"
+    fi
+
+    echo "$sourceString" > "$HOME/.zshrc"
 }
 
 echo "Do you wish to install? This will overwrite symlinks in $HOME with files in $script_dir"
