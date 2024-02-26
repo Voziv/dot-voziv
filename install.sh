bdb9969845fddffdd199ca7763df418010d07264
Lee Robert <lrobert@leerobert.ca>
2024-02-26 07:54:24 -0500

    refactor: use stow to manage files

--- install.sh
+++ install.sh
@@ -8,91 +8,12 @@ if [[ ! -d "$script_dir" ]]; then script_dir="$PWD"; fi
 runDate=$(date -d "today" +"%Y%m%d%H%M%S")
 
 function install() {
-    ensure_user_bin
-    install_symlinks
-    install_profile
-    install_zshrc
-}
-
-function ensure_user_bin() {
-    echo "Ensuring $HOME/bin is created"
-    mkdir -p "$HOME/bin"
-}
-
-function install_symlinks() {
-    # Clean up old items, useful for upgrading other machines.
-    rm_symlink "$HOME/.nvimrc" "$script_dir/.vimrc"
-    rm_symlink "$HOME/.vimrc" "$script_dir/.vimrc"
-    
-    # Add symlinks
-    do_symlink "$HOME/.config/nvim" "$script_dir/nvim"
-    do_symlink "$HOME/.gitconfig" "$script_dir/git/gitconfig"
-    do_symlink "$HOME/.tmux.conf" "$script_dir/.tmux.conf"
-    do_symlink "$HOME/.screenrc" "$script_dir/.screenrc"
-}
-
-function do_symlink() {
-    local link=$1
-    local target=$2
-
-    if [ -L "$link" ]; then
-        if [ "$(readlink -- "$link")" = "$target" ]; then
-            echo "$link already points to $target"
-            return
-        fi
-        
-        echo "Removing $link that points to `readlink $link`. Reason: Installing new link"
-        rm "$link"
-    else
-        if [ -e "$link" ]; then
-            echo "$link exists and is not a symlink. Not removing it to be safe."
-            return
-        fi
-
-        echo "Symlinking $link to $target"
-    fi
-
-    ln -s "$target" "$link"
-}
-
-function rm_symlink() {
-    local link=$1
-    local expectedTarget=$2
-
-    if [ -L "$link" ]; then
-        echo "Removing $link that points to `readlink $link`. Reason: Deprecated"
-        rm "$link"
-    fi
+    cd $script_dir
+    stow -t $HOME src
 }
 
 
-function install_profile() {
-    sourceString="source $script_dir/profile"
-    if grep -q -F "$sourceString" "$HOME/.profile"; then 
-        echo ".profile already configured. Skipping."
-        return
-    fi
-
-    echo ".profile not configured Installing .profile"
-    echo "$sourceString" >> "$HOME/.profile"
-}
-
-function install_zshrc() {
-    sourceString="source $script_dir/.zshrc"
-    if grep -q -F "$sourceString" "$HOME/.zshrc"; then 
-        echo ".zshrc already configured. Skipping."
-        return
-    fi
-    
-    echo ".zshrc not configured Installing .zshrc"
-    if [ -f "$HOME/.zshrc" ]; then
-        mv "$HOME/.zshrc" "$HOME/.zshrc.pre-dot-voziv-${runDate}"
-    fi
-
-    echo "$sourceString" > "$HOME/.zshrc"
-}
-
-echo "Do you wish to install? This will overwrite symlinks in $HOME with files in $script_dir"
+echo "Do you wish to install? This will overwrite symlinks in $HOME with files in $script_dir/src"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) install; break;;
--- src/.voziv/rc.d/05-paths.sh
+++ src/.voziv/rc.d/05-paths.sh
@@ -1,11 +1,14 @@
-#!/bin/zsh
+
 
 PATH="$HOME/.config/composer/vendor/bin:$PATH"
 PATH="$HOME/.npm-global/bin:$PATH"
 PATH="$HOME/.voziv/bin:$PATH"
 PATH="$HOME/bin:$PATH"
 PATH="$HOME/bin/nvim/bin:$PATH"
-PATH="$PATH:$HOME/dev/squadley/fs_farside/bin"
 PATH="$PATH:$HOME/.local/bin/"
 PATH="$PATH:$HOME/.linkerd2/bin"
+
+# Rancher desktop
+PATH="$PATH:$HOME/.rd/bin"
+
 export PATH
