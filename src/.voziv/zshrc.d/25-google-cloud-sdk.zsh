f3ffa45643b9dbdeb8594ea5757e06a3b63cf142
Lee Robert <25726+Voziv@users.noreply.github.com>
2025-07-04 15:32:50 -0400

    feat: load google-cloud when installed via brew

--- src/.voziv/zshrc.d/25-google-cloud-sdk.zsh
+++ src/.voziv/zshrc.d/25-google-cloud-sdk.zsh
@@ -2,3 +2,6 @@
 
 [ -f "$HOME/google-cloud-sdk/path.zsh.inc" ] && . $HOME/google-cloud-sdk/path.zsh.inc
 [ -f "$HOME/google-cloud-sdk/completion.zsh.inc" ] && . $HOME/google-cloud-sdk/completion.zsh.inc
+
+# Mac / Homebrew
+[ -f "/opt/homebrew/share/google-cloud-sdk/path.zsh.inc" ] && . /opt/homebrew/share/google-cloud-sdk/path.zsh.inc
